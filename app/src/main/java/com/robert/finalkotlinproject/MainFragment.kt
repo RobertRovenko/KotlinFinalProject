bdca687755de3e6499f74e82665f27359ce737a0
Robert <robert.ntisthlm@gmail.com>
2023-03-25 18:35:35 +0100

    Implemented Weather Api and created logic to change text based on the weather in stockholm.

--- app/src/main/java/com/robert/finalkotlinproject/MainFragment.kt
+++ app/src/main/java/com/robert/finalkotlinproject/MainFragment.kt
@@ -1,25 +1,55 @@
 package com.robert.finalkotlinproject
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.json.JSONObject
 
 
 class MainFragment : Fragment() {
 
-
+    private val weatherApiClient = WeatherApiClient("a996df0d8bc9c13f9b0e9bc81a92314a")
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
+
+
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_main, container, false)
 
+        weatherApiClient.getWeatherForCity("Stockholm") { response, error ->
+            if (error != null) {
+                Log.e(TAG, "Error fetching weather data", error)
+            } else {
+                val weatherData = JSONObject(response!!)
+                val weatherDescription = weatherData.getJSONArray("weather").getJSONObject(0).getString("description")
+                val weatherText = view.findViewById<TextView>(R.id.weatherText)
+                updateWeatherText(view.findViewById(R.id.weatherText), weatherDescription)
+            }
+        }
+
+        val validateButton = view.findViewById<Button>(R.id.validateMail)
+        val email = view.findViewById<EditText>(R.id.emailEditText).text.toString()
+
         return view
     }
 
+    private fun updateWeatherText(weatherText: TextView, description: String) {
+        activity?.runOnUiThread {
+            weatherText.text = "The weather is $description"
+        }
+    }
+
+    companion object {
+        private const val TAG = "MainFragment"
+    }
 
 }
---
+++ app/src/main/java/com/robert/finalkotlinproject/WeatherApiClient.kt
@@ -0,0 +1,38 @@
+package com.robert.finalkotlinproject
+
+import okhttp3.*
+import okhttp3.logging.HttpLoggingInterceptor
+import org.json.JSONObject
+import java.io.IOException
+
+class WeatherApiClient(private val apiKey: String) {
+    private val client = OkHttpClient.Builder()
+        .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
+        .build()
+
+    fun getWeatherForCity(cityName: String, callback: (String?, Throwable?) -> Unit) {
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=$cityName&appid=a996df0d8bc9c13f9b0e9bc81a92314a"
+
+        val request = Request.Builder()
+            .url(url)
+            .build()
+
+        client.newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                callback(null, e)
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val responseString = response.body?.string()
+                val error = responseString?.let {
+                    if (response.isSuccessful) {
+                        null
+                    } else {
+                        JSONObject(it).getString("message")
+                    }
+                }
+                callback(responseString, error?.let { Throwable(it) })
+            }
+        })
+    }
+}
