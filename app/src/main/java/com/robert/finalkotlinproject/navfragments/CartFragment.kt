0936605b6fe71fdfa2f30ffc3c449df7c46f5074
Robert <robert.ntisthlm@gmail.com>
2023-04-11 14:58:23 +0200

    When the cart is empty the payment and discount is hidden, and a text is displayed, and when products are added the text disappears and the buttons come back

--- app/src/main/java/com/robert/finalkotlinproject/navfragments/CartFragment.kt
+++ app/src/main/java/com/robert/finalkotlinproject/navfragments/CartFragment.kt
@@ -26,6 +26,11 @@ class CartFragment : Fragment() {
     private lateinit var totalCostTextView: TextView
     private lateinit var checkOutButton: Button
     private lateinit var productAdapter: ProductAdapter
+    private lateinit var discountCodeEditText: EditText
+    private lateinit var applyButton: Button
+    private lateinit var emptyCartTextView: TextView
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var bottomNavigationView: BottomNavigationView
     private var totalCost: Double = 0.0
     private var discountCode: String? = null
 
@@ -39,6 +44,14 @@ class CartFragment : Fragment() {
 
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_cart, container, false)
+
+        recyclerView = view.findViewById<RecyclerView>(R.id.cartRecyclerView)
+        totalCostTextView = view.findViewById(R.id.total_cost_textview)
+        checkOutButton = view.findViewById(R.id.pay_button)
+        discountCodeEditText = view.findViewById<EditText>(R.id.discount_code_edittext)
+        applyButton = view.findViewById<Button>(R.id.apply_discount_button)
+        emptyCartTextView = view.findViewById(R.id.empty_cart_textview)
+        bottomNavigationView = view.findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
 
         // Get the RecyclerView from the layout and set its layout manager
@@ -59,6 +72,28 @@ class CartFragment : Fragment() {
             }
             totalCostTextView.text = "$${discountedCost}"
         }
+
+
+
+        // Observe the cart items and show/hide the appropriate views
+        viewModel.products.observe(viewLifecycleOwner) { cartItems ->
+            if (cartItems.isEmpty()) {
+                recyclerView.visibility = View.GONE
+                totalCostTextView.visibility = View.GONE
+                checkOutButton.visibility = View.GONE
+                discountCodeEditText.visibility = View.GONE
+                applyButton.visibility = View.GONE
+                emptyCartTextView.visibility = View.VISIBLE
+            } else {
+                recyclerView.visibility = View.VISIBLE
+                totalCostTextView.visibility = View.VISIBLE
+                checkOutButton.visibility = View.VISIBLE
+                discountCodeEditText.visibility = View.VISIBLE
+                applyButton.visibility = View.VISIBLE
+                emptyCartTextView.visibility = View.GONE
+            }
+        }
+
 
         val discountCodeEditText = view.findViewById<EditText>(R.id.discount_code_edittext)
 
